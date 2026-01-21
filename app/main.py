95aeaf5dbdd117987453b819b3b95cacaeaab653
Lorenzo Chahid <nansheadd@gmail.com>
2026-01-21 20:23:24 +0100

    Fix ouvrir salon projet 2

--- app/main.py
+++ app/main.py
@@ -90,6 +90,7 @@ origins.append("https://lavatools.vercel.app")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
